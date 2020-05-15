3a02412db9742ca3f4a7042b1dd7229107239517
Daniel Rooker <dannyrooks@gmail.com>
2020-05-14 20:08:29 -0400

    seed data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Location.create([
+    {name: "Chattahoochie River", state: "Georgia", city: "Peachtree Corners", country: "USA"},
+    {name: "Rio Grande River", state: "Colorado", city: "Creede", country: "USA"},
+    {name: "Chattahoochie River", state: "Georgia", city: "Buford", country: "USA"},
+    {name: "Holston River", state: "Tennesee", city: "Kingsport", country: "USA"},
+    {name: "Willamette River", state: "Oregon", city: "Albany", country: "USA"},
+])
+
+Fish.create([
+    {name: "Rainbow", fish_type: "Trout"},
+    {name: "Brown", fish_type: "Trout"},
+    {name: "Striped Bass", fish_type: "Bass"},
+    {name: "Grouper", fish_type: "Bass"}
+])
+
+Location.find_by_id(1).update(fish_ids: [1, 2])
+Location.find_by_id(2).update(fish_ids: [1, 2])
+Location.find_by_id(3).update(fish_ids: [1, 2])
+Location.find_by_id(4).update(fish_ids: [1, 2, 3])
+Location.find_by_id(5).update(fish_ids: [1, 2])
+Location.find_by_id(6).update(fish_ids: [1, 2])
